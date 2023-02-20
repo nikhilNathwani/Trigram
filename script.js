1d9e5d7d19ee6517b73c869c57821dfc3cd58fee
nikhilNathwani <nnathwani36@gmail.com>
2023-02-19 22:16:53 -0800

    Updated page layout to 2-column, and began updating styling for words in the display area.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const form = document.querySelector('form');
-const input = document.querySelector('#userInput');
-const output = document.querySelector('#display-area');
+const wordInput = document.querySelector('#wordInput');
+const displayArea = document.querySelector('#display-area');
 
-input.focus(); // give focus to the input field when the page loads
+wordInput.focus(); // give focus to the input field when the page loads
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // prevent the form from refreshing page upon submission
+  // Prevent the form from refreshing page upon submission
+  event.preventDefault(); 
 
-  const text = input.value; // get the user's inputted text
-  output.textContent += text+"   "; // add text to output div
-  input.value = ''; //clear input field after submissiont
-  input.focus(); // give focus back to the input field
+  // Get the user's inputted text and confirm it meets constraints
+  const word = wordInput.value.trim();
+  //Add checks for constraints here 
+
+  //If word met constraints, add it to display area and tee up next input
+  const rowDiv = document.createElement('div');
+  rowDiv.classList.add('row');
+  for (let i = 0; i < word.length; i++) {
+    const letterDiv = document.createElement('div');
+    letterDiv.textContent = word[i];
+    letterDiv.classList.add('letter');
+    rowDiv.appendChild(letterDiv);
+  }
+  displayArea.appendChild(rowDiv);
+
+  //Clear input field and return keyboard focus to it
+  wordInput.value = '';
+  wordInput.focus();
+
+  // displayArea.textContent += word+"   "; // add text to output div
 });
+
+function addWordtoDisplayArea(word) {
+  return;
+}
