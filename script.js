023de0344499fee67707bbbbb7d032a09cd9f8f1
nikhilNathwani <nnathwani36@gmail.com>
2023-02-19 23:16:24 -0800

    Fixed the skeleton code from previous commit, now it actually compiles

--- script.js
+++ script.js
@@ -7,12 +7,23 @@ const displayArea = document.querySelector('#display-area');
 
 wordInput.focus(); // Give focus to the input field when the page loads
 
+//Where I left off:
+//Ask ChatGPT this:
+//
+// My text input needs to meet multiple criteria:
+
+// -Must be letters only (should not be able to type non-alphabet input)
+// -Must contain the substring "CAT", and I want to console log an error if the user hits submit at their inputted word doesn't contain the substring "CAT"
+// -Must be 5 letters long, and I want to console log an error if the user hits submit and their inputted word isn't 5 letters long. I also want to console.log an error if the user types more than 5 characters, even if they haven't hit submit yet
+
+// how can I handle these 3 checks in a way that keeps my code clean but also secure
+//
 
 // WORK IN PROGRESS
 // Check typed chars against real-time constraints: 
 //   1. Alpha letters only (don't let non-alpha chars be displayed at all)
 //   2. Stay within targetLength (else display 'too many letters' error)
-input.addEventListener('input', function(event) {
+form.addEventListener('input', function(event) {
   return;
 });
 
@@ -27,7 +38,7 @@ form.addEventListener('submit', (event) => {
 
   // Get the user's inputted text and confirm it meets constraints
   const word = wordInput.value.trim(); //ignore whitespace at start/end
-  const status= checkWord(word)
+  const status= checkWord(word);
   //Add checks for constraints here 
 
   //If word met constraints, add it to display area and tee up next input
@@ -38,6 +49,9 @@ form.addEventListener('submit', (event) => {
   wordInput.focus();
 });
 
+function checkWord(word) {
+  return;
+}
 
 function addWordtoDisplayArea(word) {
   //Create row div
