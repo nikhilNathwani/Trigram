0e398aa72a723ad39c7dd5bce14dbdcf917ed052
nikhilNathwani <nnathwani36@gmail.com>
2023-02-21 16:00:03 -0800

    Now only accepts input that includes the trigram and is of length 'targetlength'. Both the trigram and targetlength are currently hardcoded. Also cleaned up some comments.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const trigram= "CAT";
+const trigram= "cat";
 const targetLength= 5;
 
 const form = document.querySelector('form');
@@ -7,22 +7,12 @@ const displayArea = document.querySelector('#display-area');
 
 wordInput.focus(); // Give focus to the input field when the page loads
 
-//Where I left off:
-//Ask ChatGPT this:
-//
-// My text input needs to meet multiple criteria:
-
-// -Must be letters only (should not be able to type non-alphabet input)
-// -Must contain the substring "CAT", and I want to console log an error if the user hits submit at their inputted word doesn't contain the substring "CAT"
-// -Must be 5 letters long, and I want to console log an error if the user hits submit and their inputted word isn't 5 letters long. I also want to console.log an error if the user types more than 5 characters, even if they haven't hit submit yet
-
-// how can I handle these 3 checks in a way that keeps my code clean but also secure
-//
 
 // WORK IN PROGRESS
 // Check typed chars against real-time constraints: 
 //   1. Alpha letters only (don't let non-alpha chars be displayed at all)
 //   2. Stay within targetLength (else display 'too many letters' error)
+//   3. Don't allow copy/paste
 form.addEventListener('input', function(event) {
   return;
 });
@@ -38,19 +28,19 @@ form.addEventListener('submit', (event) => {
 
   // Get the user's inputted text and confirm it meets constraints
   const word = wordInput.value.trim(); //ignore whitespace at start/end
-  const status= checkWord(word);
-  //Add checks for constraints here 
-
-  //If word met constraints, add it to display area and tee up next input
-  addWordtoDisplayArea(word);
-
+  if(checkWord(word)) {
+      addWordtoDisplayArea(word);
+  }
   //Clear input field and return keyboard focus to it
   wordInput.value = '';
   wordInput.focus();
 });
 
+//Add checks for constraints here 
 function checkWord(word) {
-  return;
+  const correctLength= word.length == targetLength;
+  const includesTrigram= word.includes(trigram);
+  return correctLength && word.includes(trigram);
 }
 
 function addWordtoDisplayArea(word) {
