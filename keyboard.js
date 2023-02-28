76b648342895d37cd2529372d306f40adc5b184f
nikhilNathwani <nnathwani36@gmail.com>
2023-02-27 16:48:16 -0800

    Added Enter and Delete key to the on-screen keyboard. Delete is hooked up. Enter key is not; i.e. the on-screen Enter button doesn't work, but hitting the Enter key on keyboard technically still does work (because I still have an event listener for form-submit on the input field, which is still wrapped in a form. not sure if it still needs to be.

--- keyboard.js
+++ keyboard.js
@@ -8,8 +8,19 @@ keys.forEach(key => {
 });
 
 document.addEventListener("keydown", e => {
+  //If key is 'Enter', treat as a Submit action
+  if (e.key === "Enter") {
+    return;
+  }
+  //If key is 'Backspace', delete last char (if there is one)
+  if (e.key === "Backspace") {
+    if (input.value.length>0) {
+      input.value= input.value.slice(0,-1);
+    }
+  }
+
   const key = e.key.toUpperCase();
-  // console.log(e,e.key,e.key.toUpperCase());
+  console.log(e,e.key,e.key.toUpperCase());
   const matchingKey = document.querySelector(`.keyboard-key[data-letter="${key}"]`);
   // console.log(matchingKey);
   if (matchingKey) {
