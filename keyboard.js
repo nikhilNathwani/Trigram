5916a46d6c722a2a77e1906fd7c83e491491ee37
nikhilNathwani <nnathwani36@gmail.com>
2023-02-27 16:02:41 -0800

    Added ability for on-screen keyboard to work with physical keyboard presses as well. I.e. when I type a char on my keyvoard, that char now appears in the input field. Bonus side effect is that now the text input field only accepts alpha chars, since the only key presses I respond to are the ones that match the on-screen keyboard! I did also add the readonly attribute to the text input field, otherwise I could also type directly into it and then the on-screen keyboard also added letters to it, causing duplicate letters. Note: keyboard currently doesn't filter out Ctrl+C or other key presses where Ctrl is also pressed.

--- keyboard.js
+++ keyboard.js
@@ -3,21 +3,16 @@ const input = document.querySelector("input");
 
 keys.forEach(key => {
   key.addEventListener("click", () => { 
-    console.log("Before:",input.value);
     input.value += key.dataset.letter; 
-    console.log("After:",key.dataset.letter,input.value);
   });
 });
 
 document.addEventListener("keydown", e => {
   const key = e.key.toUpperCase();
+  // console.log(e,e.key,e.key.toUpperCase());
   const matchingKey = document.querySelector(`.keyboard-key[data-letter="${key}"]`);
+  // console.log(matchingKey);
   if (matchingKey) {
-    for (let i = 0; i < inputs.length; i++) {
-      if (inputs[i].value === "") {
-        inputs[i].value = key;
-        break;
-      }
-    }
+    input.value += key;
   }
 });
