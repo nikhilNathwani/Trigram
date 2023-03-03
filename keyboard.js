b45c65bb461be6d687f338a7ce5f41f59bcd43a0
nikhilNathwani <nnathwani36@gmail.com>
2023-03-03 13:07:59 -0800

    Applied Prettier formatting (from the Prettier extension) to all my files

--- keyboard.js
+++ keyboard.js
@@ -2,42 +2,40 @@ const keys = document.querySelectorAll(".keyboard-key");
 const input = document.querySelector("input");
 
 //Event listeners for clicking on the on-screen keyboard
-keys.forEach(key => {
-  key.addEventListener("click", () => { 
-    console.log("Click event:",key);
-    if(key.id=="enterKey") { 
-      handleKeyPress("Enter");
-    }
-    else if(key.id=="backspaceKey") { 
-      handleKeyPress("Backspace"); 
-    }
-    else {
-      handleKeyPress(key.dataset.letter);
-    }
-  });
+keys.forEach((key) => {
+	key.addEventListener("click", () => {
+		console.log("Click event:", key);
+		if (key.id == "enterKey") {
+			handleKeyPress("Enter");
+		} else if (key.id == "backspaceKey") {
+			handleKeyPress("Backspace");
+		} else {
+			handleKeyPress(key.dataset.letter);
+		}
+	});
 });
 
 //Event listener for typing on physical keyboard
-document.addEventListener("keydown", e => {
-  console.log("Keydown event:", e);
-  handleKeyPress(e.key);
+document.addEventListener("keydown", (e) => {
+	console.log("Keydown event:", e);
+	handleKeyPress(e.key);
 });
 
 function handleKeyPress(key) {
-  //If key is 'Enter', treat as a Submit action
-  if (key === "Enter") {
-    submitWord();
-  } 
-  else if (key === "Backspace") {
-    if (input.value.length>0) {
-      input.value = input.value.slice(0,-1);
-    }
-  } 
-  else {
-    const letter = key.toUpperCase();
-    const matchingKey = document.querySelector(`.keyboard-key[data-letter="${letter}"]`);
-    if (matchingKey) {
-      input.value += letter;
-    }
-  }
+	//If key is 'Enter', treat as a Submit action
+	if (key === "Enter") {
+		submitWord();
+	} else if (key === "Backspace") {
+		if (input.value.length > 0) {
+			input.value = input.value.slice(0, -1);
+		}
+	} else {
+		const letter = key.toUpperCase();
+		const matchingKey = document.querySelector(
+			`.keyboard-key[data-letter="${letter}"]`
+		);
+		if (matchingKey) {
+			input.value += letter;
+		}
+	}
 }
--- script.js
+++ script.js
@@ -1,96 +1,95 @@
-const trigram= "CAT";
-var targetLength= 5;
+const trigram = "CAT";
+var targetLength = 5;
 
-const form = document.querySelector('form');
-const wordInput = document.querySelector('#wordInput');
-const displayArea = document.querySelector('#displayArea');
+const form = document.querySelector("form");
+const wordInput = document.querySelector("#wordInput");
+const displayArea = document.querySelector("#displayArea");
 
 function submitWord() {
-  // Clear any existing error messages
-  clearExistingErrors();
-  // Get the user's inputted text and check if it meets constraints
-  const word = wordInput.value.trim(); //ignore whitespace at start/end
-  const [meetsConstraints, errors]= checkWord(word); 
-  if (meetsConstraints) {
-      addWordToDisplayArea(word, word.indexOf(trigram));
-      incrementTargetLength();
-  }
-  else {
-    errors.forEach(errorCode => addErrorToErrorDisplayArea(errorCode));
-  }
-  //Clear input field and return keyboard focus to it
-  wordInput.value = '';
+	// Clear any existing error messages
+	clearExistingErrors();
+	// Get the user's inputted text and check if it meets constraints
+	const word = wordInput.value.trim(); //ignore whitespace at start/end
+	const [meetsConstraints, errors] = checkWord(word);
+	if (meetsConstraints) {
+		addWordToDisplayArea(word, word.indexOf(trigram));
+		incrementTargetLength();
+	} else {
+		errors.forEach((errorCode) => addErrorToErrorDisplayArea(errorCode));
+	}
+	//Clear input field and return keyboard focus to it
+	wordInput.value = "";
 }
 
 // Returns [x,[y]] where:
 //    -x is true/false indicating whether the inputted word meets the constraints
-//    -y is an array of strings indicating which error messages to display 
+//    -y is an array of strings indicating which error messages to display
 function checkWord(word) {
-  var errorCodes= [];
-  var correctLength= word.length == targetLength;
-  var includesTrigram= word.includes(trigram);
-  //Add error codes
-  if (!correctLength) {
-    errorCodes.push("WRONG-LENGTH");
-  }
-  if (!includesTrigram) {
-    errorCodes.push("TRIGRAM-MISSING")
-  }
-  return [correctLength && includesTrigram, errorCodes];
+	var errorCodes = [];
+	var correctLength = word.length == targetLength;
+	var includesTrigram = word.includes(trigram);
+	//Add error codes
+	if (!correctLength) {
+		errorCodes.push("WRONG-LENGTH");
+	}
+	if (!includesTrigram) {
+		errorCodes.push("TRIGRAM-MISSING");
+	}
+	return [correctLength && includesTrigram, errorCodes];
 }
 
 function addErrorToErrorDisplayArea(errorCode) {
-  const errorArea = document.querySelector('#errorAlertArea');
-  const errorString = document.createElement('p');
-  errorString.textContent= lookupErrorString(errorCode);
-  errorString.classList.add('error');
-  errorArea.appendChild(errorString)
-  return;
+	const errorArea = document.querySelector("#errorAlertArea");
+	const errorString = document.createElement("p");
+	errorString.textContent = lookupErrorString(errorCode);
+	errorString.classList.add("error");
+	errorArea.appendChild(errorString);
+	return;
 }
 
 function lookupErrorString(errorCode) {
-  switch (errorCode) {
-    case "WRONG-LENGTH": 
-      return `Word must be ${targetLength} letters long.`;
-    case "TRIGRAM-MISSING":
-      return `Word must contain ${trigram}.`;
-    case "NOT-FOUND":
-      return "Word not found.";
-    default:
-      return "An error occurred.";
-  }
+	switch (errorCode) {
+		case "WRONG-LENGTH":
+			return `Word must be ${targetLength} letters long.`;
+		case "TRIGRAM-MISSING":
+			return `Word must contain ${trigram}.`;
+		case "NOT-FOUND":
+			return "Word not found.";
+		default:
+			return "An error occurred.";
+	}
 }
 
 function clearExistingErrors() {
-  const errorArea = document.querySelector('#errorAlertArea');
-  errorArea.innerHTML= "";
+	const errorArea = document.querySelector("#errorAlertArea");
+	errorArea.innerHTML = "";
 }
 
 function addWordToDisplayArea(word, trigramPosition) {
-  //Create row div
-  const rowDiv = document.createElement('div');
-  rowDiv.classList.add('word');
-  //Create letter divs for each letter in the word
-  for (let i = 0; i < word.length; i++) {
-    const letterDiv = document.createElement('div');
-    letterDiv.textContent = word[i];
-    letterDiv.classList.add('letter');
-    //If it's a trigram letter, apply trigram styling
-    if (i>=trigramPosition && i<=trigramPosition+2) {
-      letterDiv.classList.add('trigramLetter');
-    }
-    rowDiv.appendChild(letterDiv);
-  }
-  displayArea.appendChild(rowDiv);
+	//Create row div
+	const rowDiv = document.createElement("div");
+	rowDiv.classList.add("word");
+	//Create letter divs for each letter in the word
+	for (let i = 0; i < word.length; i++) {
+		const letterDiv = document.createElement("div");
+		letterDiv.textContent = word[i];
+		letterDiv.classList.add("letter");
+		//If it's a trigram letter, apply trigram styling
+		if (i >= trigramPosition && i <= trigramPosition + 2) {
+			letterDiv.classList.add("trigramLetter");
+		}
+		rowDiv.appendChild(letterDiv);
+	}
+	displayArea.appendChild(rowDiv);
 }
 
 function incrementTargetLength() {
-  //Increment value on backend
-  targetLength++; 
-  //Increment value on frontend
-  const targetLengthUI= document.querySelector('#targetLength');
-  targetLengthUI.innerText= parseInt(targetLengthUI.innerText)+1 
-  //^may want to add error handling. E.g. if targetLengthUI is 
-  //changed to a non-integer value in dev tools. Or change it 
-  //from a <p> to an immutable svg
+	//Increment value on backend
+	targetLength++;
+	//Increment value on frontend
+	const targetLengthUI = document.querySelector("#targetLength");
+	targetLengthUI.innerText = parseInt(targetLengthUI.innerText) + 1;
+	//^may want to add error handling. E.g. if targetLengthUI is
+	//changed to a non-integer value in dev tools. Or change it
+	//from a <p> to an immutable svg
 }
