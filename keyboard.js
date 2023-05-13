9b22c84cedcf322b17d00115342237a41e00b069
nikhilNathwani <nnathwani36@gmail.com>
2023-05-13 14:21:29 -0700

    Simplified code to match handwritten flow diagram. Could use more refactoring for clarity

--- keyboard.js
+++ keyboard.js
@@ -4,38 +4,91 @@ const input = document.querySelector("input");
 //Event listeners for clicking on the on-screen keyboard
 keys.forEach((key) => {
 	key.addEventListener("click", () => {
-		console.log("Click event:", key);
-		if (key.id == "enterKey") {
-			handleKeyPress("Enter");
-		} else if (key.id == "backspaceKey") {
-			handleKeyPress("Backspace");
-		} else {
-			handleKeyPress(key.dataset.letter);
-		}
+		handleKeyPress(key.dataset.keyname);
 	});
 });
 
 //Event listener for typing on physical keyboard
 document.addEventListener("keydown", (e) => {
-	console.log("Keydown event:", e);
 	handleKeyPress(e.key);
 });
 
 function handleKeyPress(key) {
-	//If key is 'Enter', treat as a Submit action
 	if (key === "Enter") {
-		submitWord();
-	} else if (key === "Backspace") {
-		if (input.value.length > 0) {
-			input.value = input.value.slice(0, -1);
+		var word = getInputWord();
+		var [isValid, errorReason] = isWordValid(word);
+		if (isValid) {
+			handleValidWord(word);
+		} else {
+			displayError(errorReason);
 		}
+		// submitWord();
+	} else if (key === "Backspace") {
+		deleteLetter();
 	} else {
-		const letter = key.toUpperCase();
-		const matchingKey = document.querySelector(
-			`.keyboard-key[data-letter="${letter}"]`
-		);
-		if (matchingKey) {
-			input.value += letter;
+		[isLetter, letter] = isLetterFromOnscreenKeyboard(key);
+		if (isLetter) {
+			addLetter(letter);
 		}
 	}
+}
+
+function getInputWord() {
+	return wordInput.value.trim(); //trim() ignores whitespace at start/end
+}
+
+function handleValidWord(word) {
+	acceptWord(word);
+	if (isLongestPossibleWord(word)) {
+		return; //Come back to this
+	} else {
+		introduceNextRound();
+	}
+}
+
+function deleteLetter() {
+	if (input.value.length > 0) {
+		input.value = input.value.slice(0, -1);
+	}
+}
+
+function addLetter(letter) {
+	input.value += letter;
+}
+
+function isLetterFromOnscreenKeyboard(key) {
+	const letter = key.toUpperCase();
+	const matchingKey = document.querySelector(
+		`.keyboard-key[data-keyname="${letter}"]`
+	);
+	return [matchingKey !== null, letter];
+}
+
+function displayError() {
+	return;
+}
+
+function acceptWord(word) {
+	// Clear any existing error messages
+	clearExistingErrors();
+	addWordToDisplayArea(word, word.indexOf(trigram));
+	incrementTargetLength();
+	//Clear input field and return keyboard focus to it
+	wordInput.value = "";
+}
+
+function introduceNextRound() {
+	return;
+}
+
+function isWordValid(word) {
+	const [meetsConstraints, errors] = checkWord(word);
+	if (!meetsConstraints) {
+		errors.forEach((errorCode) => addErrorToErrorDisplayArea(errorCode));
+	}
+	return [meetsConstraints, ""];
+}
+
+function isLongestPossibleWord(word) {
+	return false;
 }
