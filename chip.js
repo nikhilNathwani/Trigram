8ba674d1ec388c434c4fd3b578aea62cb7811cfe
nikhilNathwani <nnathwani36@gmail.com>
2023-05-23 00:57:42 -0700

    Achieved the desired underline effectgit status In such an elegant way. Only uses js for the width of the input box because it depends on targetLength. The rest is all in css, using repeating-linear-gradient with the background image. The challenge was getting the dash pattern to line up exactly with the letters, so that the letters sit perfectly on top of the dashes. Initially I was setting the gradient color stops to the font size of the letters (2rem), but that's the height of the letters not the width. So then I started doing all kinds of crazy html math to calculate the pixel width of the letters when their font size is 2rem, but this still wasnt consistently lining things up, lot of off-by-one-pixel issues that compounded as words get longer. Learned about the 'ch' unit, which gives the width of '0' (zero) typed in the current font, and then I changed the font to monospace so that all chars have the same width as 0. Now all the types chars line up perfectly atop each underline.

--- chip.js
+++ chip.js
@@ -2,10 +2,10 @@ const gameArea = document.querySelector("#gameArea");
 const displayArea = document.querySelector("#displayArea");
 const gameDisplayGrid = document.querySelector("#gameDisplayGrid");
 const errorDialog = document.querySelector("#errorString");
-const computedStyle = getComputedStyle(document.documentElement);
-const letterWidth = "21px"; //computedStyle.getPropertyValue("--letterWidth");
-const letterGap = computedStyle.getPropertyValue("--letterGap");
+const rootStyles = getComputedStyle(document.documentElement);
+const letterGap = rootStyles.getPropertyValue("--letterGap");
 //
+
 function changeCurrentInputFieldToStaticDisplay(word) {
 	const currentInputField = document.querySelector(
 		`input#wordInputBox_${targetLength - 1}`
@@ -46,21 +46,14 @@ function addInputDivToDisplay() {
 	//Create input field
 	const wordInputBox = document.createElement("input");
 	// Set the attributes
+	wordInputBox.style.width = `calc((${targetLength} * (1ch + ${letterGap})))`;
 	wordInputBox.type = "text";
 	wordInputBox.name = "wordInput";
 	wordInputBox.id = "wordInput";
-	// wordInputBox.placeholder = "Enter word here";
-	// inputElement.readOnly = true;
+	wordInputBox.maxLength = targetLength;
 	wordInputBox.classList.add("wordInputBox");
 	wordInputBox.id = "wordInputBox_" + targetLength;
 	wordInputCell.appendChild(wordInputBox);
-
-	//Create dashed underline
-	const dashedUnderline = document.createElement("div");
-	dashedUnderline.style.height = "2px";
-	dashedUnderline.style.width = `calc((${targetLength} * (${letterWidth} + ${letterGap})) - ${letterGap})`;
-	dashedUnderline.style.backgroundImage = `repeating-linear-gradient(to right, black 0rem, black ${letterWidth}, white ${letterWidth}, white calc(${letterWidth} + ${letterGap}))`;
-	wordInputCell.appendChild(dashedUnderline);
 
 	gameDisplayGrid.appendChild(rowDiv);
 	scrollDisplayToBottom();
