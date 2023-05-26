a256fe46af055251c0a889b153164ad7434fc2f1
nikhilNathwani <nnathwani36@gmail.com>
2023-05-26 10:28:10 -0700

    Removed extraneous functions

--- chip.js
+++ chip.js
@@ -48,7 +48,6 @@ function addInputDivToDisplay() {
 	wordInputBox.style.width = `calc((${targetLength} * (1ch + ${letterGap})))`;
 	wordInputBox.type = "text";
 	wordInputBox.name = "wordInput";
-	wordInputBox.id = "wordInput";
 	wordInputBox.maxLength = targetLength;
 	wordInputBox.classList.add("wordInputBox");
 	wordInputBox.id = "wordInputBox_" + targetLength;
@@ -82,33 +81,6 @@ function createWordInputCellInnerHTML(word) {
 	const preTrigram = word.slice(0, trigramPosition);
 	const postTrigram = word.slice(trigramPosition + 3);
 	return preTrigram + "<span>" + trigram + "</span>" + postTrigram;
-}
-
-function addWordToDisplayArea(word) {
-	//Create row div
-	const rowDiv = document.createElement("div");
-	rowDiv.classList.add("gameDisplayRow");
-
-	//Create cell containing target length
-	const targetLengthCell = document.createElement("div");
-	targetLengthCell.classList.add("targetLengthCell");
-	targetLengthCell.id = "targetLengthCell_" + targetLength;
-	targetLengthCell.textContent = targetLength;
-	rowDiv.appendChild(targetLengthCell);
-
-	//Create cell containing completed word
-	const wordInputCell = document.createElement("div");
-	wordInputCell.classList.add("wordInputCell");
-	wordInputCell.id = "wordInputCell_" + targetLength;
-	wordInputCell.innerHTML = createWordInputCellInnerHTML(word);
-	rowDiv.appendChild(wordInputCell);
-
-	//Apply styling class to trigram span
-	const trigramSpan = wordInputCell.querySelector("span");
-	trigramSpan.classList.add("trigramSubstring");
-
-	gameDisplayGrid.appendChild(rowDiv);
-	scrollDisplayToBottom();
 }
 
 function displayError(word, errorReason) {
