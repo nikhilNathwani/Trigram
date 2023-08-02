ee2163004fc2d2b15cbf7ba137609ffc100d40d5
nikhilNathwani <nnathwani36@gmail.com>
2023-08-02 15:05:30 -0700

    Starting over, now planning deliberately in a Google Doc, feeling more hopeful with this approach: https://docs.google.com/document/d/121ecbSdc9IGkPLFhqHbfOb8pdPTewPbkVeitMIZIW-U/edit?pli=1

--- public/js/keyboardManager.js
+++
@@ -1,8 +0,0 @@
-const keys = document.querySelectorAll(".keyboard-key");
-
-//Create event listeners for clicking on the on-screen keyboard
-// keys.forEach((key) => {
-// 	key.addEventListener("click", () => {
-// 		handleKeyPress(key.dataset.keyname);
-// 	});
-// });
