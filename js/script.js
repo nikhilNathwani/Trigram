54f3160efb34f0a63ae0c0191f6e1f07b3db00f1
nikhilNathwani <nnathwani36@gmail.com>
2023-08-04 13:32:10 -0700

    Added skeleton html to start building the UI_STATE code on top of. And moved a couple functions to a new alertHandler.js file which will eventually manage all alerts and associated state

--- js/script.js
+++ js/script.js
@@ -86,19 +86,3 @@ function endGame() {
 }
 
 // BELONGS IN OTHER FILES -------------------------------------------------- //
-function wordListContains(word) {
-	return true;
-}
-
-function displayError() {
-	var word = GAME_STATE.lettersProvided[GAME_STATE.wordLength_current];
-	var errorReason =
-		word.length != GAME_STATE.wordLength_current
-			? `Word not ${GAME_STATE.wordLength_current} letters long`
-			: !word.includes(GAME_STATE.trigram)
-			? `Word doesn't contain ${GAME_STATE.trigram}`
-			: !wordListContains(word)
-			? "Not in word list"
-			: "Unknown error";
-	console.log("Error reason:", errorReason);
-}
