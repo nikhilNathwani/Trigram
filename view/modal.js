f04ca647d1b8edbfc2b95eabbc1b2a62fd99e88d
nikhilNathwani <nnathwani36@gmail.com>
2024-05-02 15:45:25 -0400

    added landscape checks post-load in modal.js

--- view/modal.js
+++ view/modal.js
@@ -193,6 +193,26 @@ function hideYouWinScreen() {
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 //
 
+// Check if the device is in landscape mode
+function isLandscape() {
+	return window.innerHeight < window.innerWidth;
+}
+
+// Check orientation when the window is resized,
+// and show noLandscape screen accordingly
+window.addEventListener("resize", function () {
+	if (isLandscape()) {
+		document.getElementById("noLandscapeScreen").style.display = "block";
+	} else {
+		document.getElementById("noLandscapeScreen").style.display = "none";
+	}
+});
+
+// Initial check on page load
+if (isLandscape()) {
+	document.getElementById("noLandscapeScreen").style.display = "block";
+}
+
 //////////////////////////////////////////////////////
 // HELPER FUNCTIONS --------------------------------//
 //////////////////////////////////////////////////////
